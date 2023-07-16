0e9e657258dd1796c357eb9e1d4191959deaa823
andrewsmith2374 <54158695+andrewsmith2374@users.noreply.github.com>
2023-07-15 18:16:31 -0700

    Initial Commit

---
+++ GradeCalc/GradeCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradeCalcApp.swift
+//  GradeCalc
+//
+//  Created by Andrew Smith on 2023.07.15.
+//
+
+import SwiftUI
+
+@main
+struct GradeCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
